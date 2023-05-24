f500aa20fe7138a63122bd937aa2b9770369feff
caaallum <callum_speirs@protonmail.com>
2023-05-24 21:32:44 +0100

    Added license header

--- include/cutil.h
+++ include/cutil.h
@@ -1,9 +1,26 @@
+/**********************************************************
+ * This file is part of cutil.
+ *
+ * cutil is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License 
+ * as published by the Free SoftwareFoundation, either version 3 
+ * of the License, or (at your option) any later version.
+ * 
+ * cutil is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with cutil. If not, see <https://www.gnu.org/licenses/>.
+ **********************************************************/
+
 #ifndef __CUTIL_H
 #define __CUTIL_H
 
 #include <cutil/map.h>
-#include <cutil/vector.h>
-#include <cutil/string.h>
 #include <cutil/stack.h>
+#include <cutil/string.h>
+#include <cutil/vector.h>
 
 #endif /* __CUTIL_H */
