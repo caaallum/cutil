63b49e4d75aa9252181aac1de24ac4f549adc6e8
caaallum <callum_speirs@protonmail.com>
2023-07-30 00:02:36 +0100

    Update check_map with char tests

--- check/check_map.c
+++ check/check_map.c
@@ -1,17 +1,17 @@
 /**********************************************************
  * This file is part of cutil.
  *
- * cutil is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License 
- * as published by the Free SoftwareFoundation, either version 3 
+ * cutil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License
+ * as published by the Free SoftwareFoundation, either version 3
  * of the License, or (at your option) any later version.
- * 
- * cutil is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * cutil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
+ *
+ * You should have received a copy of the GNU General Public License
  * along with cutil. If not, see <https://www.gnu.org/licenses/>.
  **********************************************************/
 
@@ -101,16 +101,84 @@ START_TEST(test_map_get_string_many) {
 }
 END_TEST
 
+START_TEST(test_map_add_char) {
+  map_t *map;
+
+  map = map_new();
+  int a = 1;
+
+  map_add(map, 'k', &a);
+
+  ck_assert_int_eq(map_total(map), 1);
+
+  map_destroy(map);
+}
+END_TEST
+
+START_TEST(test_map_add_char_many) {
+  map_t *map;
+  int a = 1, b = 2, c = 3, d = 4;
+
+  map = map_new();
+
+  map_add(map, 'a', &a);
+  ck_assert_int_eq(map_total(map), 1);
+  map_add(map, 'b', &b);
+  ck_assert_int_eq(map_total(map), 2);
+  map_add(map, 'c', &c);
+  ck_assert_int_eq(map_total(map), 3);
+  map_add(map, 'd', &d);
+  ck_assert_int_eq(map_total(map), 4);
+
+  map_destroy(map);
+}
+END_TEST
+
+START_TEST(test_map_get_char) {
+  map_t *map;
+  int a = 1;
+
+  map = map_new();
+
+  map_add(map, 'a', &a);
+
+  ck_assert_int_eq(a, map_get_int(map, 'a'));
+
+  map_destroy(map);
+}
+END_TEST
+
+START_TEST(test_map_get_char_many) {
+  map_t *map;
+  int a = 1, b = 2, c = 3, d = 4;
+
+  map = map_new();
+
+  map_add(map, 'a', &a);
+  ck_assert_int_eq(a, map_get_int(map, 'a'));
+
+  map_add(map, 'b', &b);
+  ck_assert_int_eq(b, map_get_int(map, 'b'));
+
+  map_add(map, 'c', &c);
+  ck_assert_int_eq(c, map_get_int(map, 'c'));
+
+  map_add(map, 'd', &d);
+  ck_assert_int_eq(d, map_get_int(map, 'd'));
+
+  map_destroy(map);
+}
+END_TEST
+
 Suite *map_suite(void) {
   Suite *s;
-  TCase *tc_core, *tc_string;
+  TCase *tc_core, *tc_string, *tc_char;
 
   s = suite_create("map");
 
   /* Core */
   tc_core = tcase_create("Core");
   tcase_add_test(tc_core, test_map_new);
-
   suite_add_tcase(s, tc_core);
 
   /* String */
@@ -119,8 +187,14 @@ Suite *map_suite(void) {
   tcase_add_test(tc_string, test_map_add_string_many);
   tcase_add_test(tc_string, test_map_get_string);
   tcase_add_test(tc_string, test_map_get_string_many);
-
   suite_add_tcase(s, tc_string);
+
+  tc_char = tcase_create("Char");
+  tcase_add_test(tc_char, test_map_add_char);
+  tcase_add_test(tc_char, test_map_add_char_many);
+  tcase_add_test(tc_char, test_map_get_char);
+  tcase_add_test(tc_char, test_map_get_char_many);
+  suite_add_tcase(s, tc_char);
 
   return s;
 }
